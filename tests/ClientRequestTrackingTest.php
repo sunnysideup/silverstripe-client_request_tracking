d80149850bd6d98a9f75ce6987bd4adde859189d
sunnysideup <sunnysideupdevs@gmail.com>
2017-01-03 14:00:05 +1300

    PATCH: module clean-up

---
+++ tests/ClientRequestTrackingTest.php
@@ -0,0 +1,13 @@
+<?php
+class ClientRequestTrackingTest extends SapphireTest {
+
+	protected $usesDatabase = false;
+
+	protected $requiredExtensions = array();
+
+	public function testMyMethod() {
+		$this->assertEquals(1, 1);
+	}
+
+}
+
